0debd8a4d1ef4b7486ef19a6c193415038b6260d
Piyaphon Wu <wupiyaphon@gmail.com>
2023-11-13 15:32:04 +0700

    add new post icon and task list with styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,34 @@ import classNames from "classnames"
 import Image from "next/image"
 
 export default function Home() {
+  let title: string | null
+
+  let content: string | null
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="my-4 text-center font-semibold text-gray-900 dark:text-white">
         Todo List
       </div>
-      <div className="relative mx-auto flex w-full max-w-[48rem] flex-row justify-center rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-        <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
-          <h6 className="mb-4 block p-6 pb-0 font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased">
-            tasks
-          </h6>
+      <div className="relative mx-auto flex w-full max-w-[70rem] flex-row justify-center rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <div className="relative m-0 w-2/6 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+          <div className="flex flex-row">
+            <h6 className="mb-4 basis-3/4 p-6 pb-0 font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased">
+              tasks
+            </h6>
+            <div className="align mb-4 grid basis-1/4 justify-end p-6 pb-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="#ec4899"
+                className="inline-block h-5 w-5 cursor-pointer hover:fill-pink-600"
+              >
+                <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+                <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+              </svg>
+            </div>
+          </div>
+
           {task(0, "My Homework")}
         </div>
         <div className="p-6">
@@ -64,8 +82,10 @@ function task(index: number, task: string) {
         "pt-0": index === 0 ? true : false,
       })}
     >
-      {task}
-      <hr className="mt-2" />
+      <div className="cursor-pointer rounded-md hover:bg-slate-100">
+        <p className="p-2">{task}</p>
+        <hr />
+      </div>
     </div>
   )
 }
